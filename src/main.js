b90cdd4da74e5934374ac4ae61ad2e24e7b69133
Jenny Strømmen <jennys1994@gmail.com>
2022-01-09 13:14:59 +0100

    Try to fix heroku port

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${ PORT }`);
+});
