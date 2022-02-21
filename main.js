6da986dd3878f5d4c5683cb6eb18efb3b9cca8a6
Jenny Strømmen <jennys1994@gmail.com>
2022-02-21 13:30:56 +0100

    Run program in textarea

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ app.get('/equalsoperator', (req, res) => res.render('equalsOp.html'));
 app.get('/semicolon', (req, res) => res.render('semiAfterIf.html'));
 app.get('/bitwiseoperator', (req, res) => res.render('bitwiseoperator.html'));
 app.get('/integerdivision', (req, res) => res.render('integerdivision.html'));
+app.get('/firstTask', (req, res) => res.render('firstTask.html'));
 
 app.listen(port, () => console.log(`Listening on ${ port }`));
 
